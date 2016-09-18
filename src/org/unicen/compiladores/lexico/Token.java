49f501a3eccce685881c9c0358472460393bee8e
ferando <ferando@DESKTOP-4V1LM0N>
2016-09-18 16:14:55 -0300

    gramatica version 2 y se anexa la actual. Funcionalidad de nro de linea para Token.

--- src/org/unicen/compiladores/lexico/Token.java
+++ src/org/unicen/compiladores/lexico/Token.java
@@ -2,37 +2,50 @@ package org.unicen.compiladores.lexico;
 
 public class Token {
 	
-	private String lexema;
-    private String nombre;
-    
-    public Token(){
-    	lexema = "";
-    	nombre = "";
-    }
-    
-    public void agregarLexema(String c){
-        this.lexema += c;
-    }
-    
-    public String obtenerLexema(){
-        return this.lexema;
-    }
-    
-    public void setearLexema(String l){
-        this.lexema = l;
-    }
-    
-    public void limpiarLexema(){
-        this.lexema = "";
-    }
-    
-    public void setearNombre(String n){
-        this.nombre = n;
-    }
-    
-    public String obtenerNombre(){
-        return this.nombre;
-    }
+	 private String lexema = "";
+	 private String nombre = "";
+	 private int linea ;
+
+	 public Token(){
+
+	 }
+
+	 public void agregarLexema(String c){
+		 this.lexema += c;
+	 }
+
+	 public String obtenerLexema(){
+		 return this.lexema;
+	 }
+
+	 public void setearLexema(String l){
+		 this.lexema = l;
+	 }
+
+	 public void limpiarLexema(){
+		 this.lexema = "";
+	 }
+
+	 public void setearNombre(String n){
+		 this.nombre = n;
+	 }
+
+	 public String obtenerNombre(){
+		 return this.nombre;
+	 }   
+
+	 public void quitarprefijo(){
+		 String value = this.lexema;
+		 this.setearLexema(value.substring(2));
+	 }
+
+	 public void setearlinea(int l){
+		 this.linea = l;
+	 }
+
+	 public int getlinea(){
+		 return this.linea;
+	 }
     public String toString(){
     	return "nombre: "+nombre+" lexema: "+lexema;
     }
